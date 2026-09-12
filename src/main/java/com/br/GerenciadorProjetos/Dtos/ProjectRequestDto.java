49d844083e5d759b5cbf612b6bb5d670abee37b2
Guilherme-Urnau <guilherme.urnau@protonmail.com>
2026-09-12 14:59:41 -0300

    criado dto de requisição para entidade de projetos.

--- src/main/java/com/br/GerenciadorProjetos/Dtos/ProjectRequestDto.java
+++ src/main/java/com/br/GerenciadorProjetos/Dtos/ProjectRequestDto.java
@@ -2,30 +2,39 @@ package com.br.GerenciadorProjetos.Dtos;
 
 import com.br.GerenciadorProjetos.Entity.ProjectMember;
 import com.br.GerenciadorProjetos.Enums.ProjectStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
-public record ProjectDto(
+public record ProjectRequestDto(
 
+        @NotNull
         Long projectId,
 
+        @NotBlank
         String name,
 
+        @NotNull
         LocalDate startDate,
 
+        @NotNull
         LocalDate estimatedEndDate,
 
         LocalDate ActualEndDate,
 
+        @NotNull
         BigDecimal totalBudget,
 
+        @NotBlank
         String description,
 
+        @NotNull
         Long manager,
 
-        ProjectStatus projectStatus,
-
+        @NotEmpty
         List<ProjectMember> members
 ) {}
