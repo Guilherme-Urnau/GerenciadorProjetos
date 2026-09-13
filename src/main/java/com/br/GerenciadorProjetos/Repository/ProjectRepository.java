4f318bf05cf1dff63ec14bd26a2225b4a3951370
Guilherme-Urnau <guilherme.urnau@protonmail.com>
2026-09-13 19:33:59 -0300

    ProjectRepository agora extende JpaExpecificationExecutor

--- src/main/java/com/br/GerenciadorProjetos/Repository/ProjectRepository.java
+++ src/main/java/com/br/GerenciadorProjetos/Repository/ProjectRepository.java
@@ -2,8 +2,9 @@ package com.br.GerenciadorProjetos.Repository;
 
 import com.br.GerenciadorProjetos.Entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project,Long> {
+public interface ProjectRepository extends JpaRepository<Project,Long>, JpaSpecificationExecutor<Project> {
 }
