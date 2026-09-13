12b04f13e3d17100bf120d0e15ba97f5381b7c7f
Guilherme-Urnau <guilherme.urnau@protonmail.com>
2026-09-12 22:25:27 -0300

    Implementados metodos CRUD. Regras de negócio ainda não foram implementadas.

--- src/main/java/com/br/GerenciadorProjetos/Services/ProjectService.java
+++ src/main/java/com/br/GerenciadorProjetos/Services/ProjectService.java
@@ -8,30 +8,39 @@ import com.br.GerenciadorProjetos.Mappers.ProjectMapper;
 import com.br.GerenciadorProjetos.Repository.ProjectRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class ProjectService {
+
+    //TODO VALIDAR REGRAS DE NEGOCIO
 
     private final ProjectRepository repository;
     private final ProjectMapper mapper;
 
-    public Page<ProjectRequestDto> getProjectByFilter(ProjectFilterDto filter, Pageable pageable){
-        return null;
+    public ProjectResponseDto getProjectById(Long projectId) {
+        Project entity = repository.findById(projectId).get();
+        return mapper.toResponseDto(entity);
+    }
+
+    public Page<ProjectResponseDto> getProjectByFilter(ProjectFilterDto filter, Pageable pageable){
+
+        return null; //todo
     }
 
     public ProjectResponseDto createProject(ProjectRequestDto requestDto){
         Project entity = mapper.toProjectEntity(requestDto);
-        ProjectResponseDto responseDto = mapper.toResponseDto(repository.save(entity));
-        return responseDto;
+        repository.save(entity);
+        return mapper.toResponseDto(entity);
     }
 
     public ProjectResponseDto updateProject(Long projectId, ProjectRequestDto requestDto){
-        Project entity = repository.findById(projectId).orElseThrow(() ->
-                new EntityNotFoundException("Projeto não encontrado, atualização cancelada."));
+        Project entity = repository.findById(projectId).orElseThrow(
+                () -> new EntityNotFoundException("Projeto não encontrado, atualização cancelada."));
         mapper.updateEntityFromDto(requestDto,entity);
 
         repository.save(entity);
@@ -39,10 +48,11 @@ public class ProjectService {
     }
 
     public void deleteProject(Long projectId){
-        Project entity = repository.findById(projectId).orElseThrow(() -> new EntityNotFoundException("Projeto não encontrado, nada para deletar"));
+        Project entity = repository.findById(projectId).orElseThrow(
+                () -> new EntityNotFoundException("Projeto não encontrado, nada para deletar"));
         repository.delete(entity);
-        return;
     }
+
 
 
 }
