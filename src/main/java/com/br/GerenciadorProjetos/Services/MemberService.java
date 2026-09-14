703e528bfcda99e350247d81c990adb0c8778ec4
Guilherme-Urnau <guilherme.urnau@protonmail.com>
2026-09-14 17:29:49 -0300

    Criado metodo para validação de "papel" do membro.

--- src/main/java/com/br/GerenciadorProjetos/Services/MemberService.java
+++ src/main/java/com/br/GerenciadorProjetos/Services/MemberService.java
@@ -3,6 +3,7 @@ package com.br.GerenciadorProjetos.Services;
 import com.br.GerenciadorProjetos.Dtos.MemberRequestDto;
 import com.br.GerenciadorProjetos.Dtos.MemberResponseDto;
 import com.br.GerenciadorProjetos.Entity.Member;
+import com.br.GerenciadorProjetos.Enums.MemberRole;
 import com.br.GerenciadorProjetos.Mappers.memberMapper;
 import com.br.GerenciadorProjetos.Repository.MemberRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -10,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 @RequiredArgsConstructor
@@ -29,8 +32,16 @@ public class MemberService {
         return memberMapper.toMemberResponseDto(entity);
     }
 
-    public MemberResponseDto createMember(MemberRequestDto requestDto) {
+    public MemberResponseDto createMember(MemberRequestDto requestDto) throws Exception {
+        verifyRole(requestDto.role());
+
         Member entity = memberRepository.save(memberMapper.toMemberEntity(requestDto));
         return memberMapper.toMemberResponseDto(entity);
+    }
+
+    private void verifyRole(String role) throws Exception {
+        Boolean isValid = Arrays.stream(MemberRole.values()).anyMatch(r -> r.name().equalsIgnoreCase(role));
+        if (!isValid) throw new Exception("Role incorreto");
+        //todo criar nova exceçao e colocar no controller advice
     }
 }
