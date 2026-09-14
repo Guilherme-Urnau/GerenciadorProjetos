8f76b6308e316d0ae1dab10d570a6ef2c7707a57
Guilherme-Urnau <guilherme.urnau@protonmail.com>
2026-09-14 15:28:12 -0300

    Criado MemberService

---
+++ src/main/java/com/br/GerenciadorProjetos/Services/MemberService.java
@@ -0,0 +1,36 @@
+package com.br.GerenciadorProjetos.Services;
+
+import com.br.GerenciadorProjetos.Dtos.MemberRequestDto;
+import com.br.GerenciadorProjetos.Dtos.MemberResponseDto;
+import com.br.GerenciadorProjetos.Entity.Member;
+import com.br.GerenciadorProjetos.Mappers.memberMapper;
+import com.br.GerenciadorProjetos.Repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final memberMapper memberMapper;
+
+    public Page<MemberResponseDto> getAllMembers(Pageable pageable) {
+        return memberRepository.findAll(pageable).map(memberMapper::toMemberResponseDto);
+    }
+
+    public MemberResponseDto getMemberById(Long memberId) {
+        Member entity = memberRepository.findById(memberId)
+                .orElseThrow(() -> new EntityNotFoundException("Membro não encontrado"));
+
+        return memberMapper.toMemberResponseDto(entity);
+    }
+
+    public MemberResponseDto createMember(MemberRequestDto requestDto) {
+        Member entity = memberRepository.save(memberMapper.toMemberEntity(requestDto));
+        return memberMapper.toMemberResponseDto(entity);
+    }
+}
