935437b60740f42d696b86241aa75e6d8846b349
Guilherme-Urnau <guilherme.urnau@protonmail.com>
2026-09-14 15:19:20 -0300

    criado memberMapper.

---
+++ src/main/java/com/br/GerenciadorProjetos/Mappers/memberMapper.java
@@ -0,0 +1,13 @@
+package com.br.GerenciadorProjetos.Mappers;
+
+import com.br.GerenciadorProjetos.Dtos.MemberRequestDto;
+import com.br.GerenciadorProjetos.Entity.Member;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface memberMapper {
+
+    Member toMemberEntity(MemberRequestDto dto);
+
+
+}
