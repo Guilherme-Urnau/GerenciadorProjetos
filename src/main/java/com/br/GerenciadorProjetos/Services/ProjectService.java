ec2901a075b6a866652026cf258c3e46d63ffb67
Guilherme-Urnau <guilherme.urnau@protonmail.com>
2026-09-14 15:47:12 -0300

    Criado novo metodo para conectar projeto com a entidade de membro.

--- src/main/java/com/br/GerenciadorProjetos/Services/ProjectService.java
+++ src/main/java/com/br/GerenciadorProjetos/Services/ProjectService.java
@@ -3,12 +3,13 @@ package com.br.GerenciadorProjetos.Services;
 import com.br.GerenciadorProjetos.Dtos.ProjectRequestDto;
 import com.br.GerenciadorProjetos.Dtos.ProjectFilterDto;
 import com.br.GerenciadorProjetos.Dtos.ProjectResponseDto;
+import com.br.GerenciadorProjetos.Entity.Member;
 import com.br.GerenciadorProjetos.Entity.Project;
 import com.br.GerenciadorProjetos.Mappers.ProjectMapper;
+import com.br.GerenciadorProjetos.Repository.MemberRepository;
 import com.br.GerenciadorProjetos.Repository.ProjectRepository;
 import com.br.GerenciadorProjetos.Repository.Specification.ProjectSpecification;
 import jakarta.persistence.EntityNotFoundException;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,40 +21,46 @@ public class ProjectService {
 
     //TODO VALIDAR REGRAS DE NEGOCIO
 
-    private final ProjectRepository repository;
+    private final ProjectRepository projectRepository;
+    private final MemberRepository memberRepository;
     private final ProjectMapper mapper;
 
     public ProjectResponseDto getProjectById(Long projectId) {
-        Project entity = repository.findById(projectId).get();
+        Project entity = projectRepository.findById(projectId).get();
         return mapper.toResponseDto(entity);
     }
 
     public Page<ProjectResponseDto> getProjectByFilter(ProjectFilterDto filter, Pageable pageable){
-        return repository.findAll(ProjectSpecification.entityFilters(filter),pageable)
+        return projectRepository.findAll(ProjectSpecification.entityFilters(filter),pageable)
                 .map(mapper::toResponseDto);
     }
 
     public ProjectResponseDto createProject(ProjectRequestDto requestDto){
         Project entity = mapper.toProjectEntity(requestDto);
-        repository.save(entity);
+        connectManager(requestDto,entity);
+        projectRepository.save(entity);
         return mapper.toResponseDto(entity);
     }
 
     public ProjectResponseDto updateProject(Long projectId, ProjectRequestDto requestDto){
-        Project entity = repository.findById(projectId).orElseThrow(
+        Project entity = projectRepository.findById(projectId).orElseThrow(
                 () -> new EntityNotFoundException("Projeto não encontrado, atualização cancelada."));
         mapper.updateEntityFromDto(requestDto,entity);
-
-        repository.save(entity);
+        connectManager(requestDto,entity);
+        projectRepository.save(entity);
         return mapper.toResponseDto(entity);
     }
 
     public void deleteProject(Long projectId){
-        Project entity = repository.findById(projectId).orElseThrow(
+        Project entity = projectRepository.findById(projectId).orElseThrow(
                 () -> new EntityNotFoundException("Projeto não encontrado, nada para deletar"));
-        repository.delete(entity);
+        projectRepository.delete(entity);
     }
 
-
+    private void connectManager(ProjectRequestDto requestDto, Project projectEntity){
+         Member memberEntity = memberRepository.findById(requestDto.manager())
+                 .orElseThrow(() -> new EntityNotFoundException("Gerente não encontrado."));
+         projectEntity.setManager(memberEntity);
+    }
 
 }
