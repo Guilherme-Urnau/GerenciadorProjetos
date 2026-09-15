151557a2cc67a585f22fe25c9ce01ce45fd23998
Guilherme-Urnau <guilherme.urnau@protonmail.com>
2026-09-15 14:43:50 -0300

    Implementado metodo com query pra verificar quantia de projetos por membro no banco, evitando carregar muitos dados e verificar em memória.

--- src/main/java/com/br/GerenciadorProjetos/Repository/ProjectRepository.java
+++ src/main/java/com/br/GerenciadorProjetos/Repository/ProjectRepository.java
@@ -3,8 +3,19 @@ package com.br.GerenciadorProjetos.Repository;
 import com.br.GerenciadorProjetos.Entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project,Long>, JpaSpecificationExecutor<Project> {
+
+    @Query("""
+        SELECT COUNT(p) FROM Project p
+        JOIN p.members m
+        WHERE m.id = :memberId
+        AND p.projectStatus NOT IN ("ENCERRADO","CANCELADO")
+    """)
+    Long isValidMember(@Param("memberId") Long memberId);
+
 }
